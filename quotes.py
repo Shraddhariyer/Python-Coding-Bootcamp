188569009695ee5e542a07d13cfb56531affeee8
Shraddhariyer <130347830+Shraddhariyer@users.noreply.github.com>
2026-05-30 19:36:05 +0530

    Initial Project Created

---
+++ quotes.py
@@ -0,0 +1,12 @@
+import random
+
+quotes=[
+    "Happiness is the key to success.",
+    "Consistency is the key.",
+    "Never give up.",
+    "Believe in yourself."
+]
+
+quote=random.choice(quotes)
+print("\nMOTIVATIONAL QUOTES\n")
+print(quote)
