55aa5d7c40585f26b0b4bbb1257b4f18d0b60da8
Shraddha Iyer <130347830+Shraddhariyer@users.noreply.github.com>
2026-06-06 16:07:07 +0530

    Add files via upload
    Day 16 Completed

---
+++ day16/quotes.py
@@ -0,0 +1,12 @@
+import random
+
+quotes=[
+    "Happiness is the key to success.",
+    "Belive in yourself",
+    "Consistency is the key",
+    "Dream big, start small.",
+    "Start from today itself"
+]
+
+quote=random.choice(quotes)
+print("Today's quote:",quote)
